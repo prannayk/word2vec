d94fe98f4fd4b4c970fcb3da511669371f92a62c
Prannay Khosla <prannay.khosla@gmail.com>
2017-02-21 22:53:42 +0530

    Starting creation of neural nets

---
+++ model/hierarchial_model.lua
@@ -0,0 +1,10 @@
+require 'torch'
+require 'nn'
+
+local hiererchial_model = {}
+function hiererchial_model.create_net(input_size, dropout)
+    dropout = dropout or 0
+    
+    local inputs = {}
+    
+end
