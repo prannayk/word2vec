7b009ea8c34744b2220996ef433e0912fe1311dc
Prannay Khosla <prannay.khosla@gmail.com>
2017-02-26 00:26:00 +0530

    Update on model

---
+++ model/README.md
@@ -0,0 +1,8 @@
+Given models are generated according to the following characterstics:
+
+1. Hierarchial Model :  Models the entire skip window as a result, and trains for cross entropy
+2. Hierarchial Sampling Model :  Models one batch with a one to one mapping with the context
+3. Convolutional Hierarch\y : Sizes down using convolution on one dimensional systems
+
+### NOTE
+Last 2 are under construction and have not yet, been merged with master
--- model/hierarchial_model.lua
+++ model/hierarchial_model.lua
@@ -1,8 +1,9 @@
 require 'torch'
 require 'nn'
 
-local hiererchial_model = {}
-function hiererchial_model.create_net(input_size, dropout,vocab_size)
+local hierarchial_model = {}
+-- init function to be added
+function hiererchial_model.create_net(input_size, dropout,vocab_size, num_layers)
     skip_window = self.skip_window or 2
     vector_size = 0
     logrange = math.floor(math.log(vocab_size))
@@ -17,9 +18,29 @@ function hiererchial_model.create_net(input_size, dropout,vocab_size)
     numlayers = self.numlayers or numlayers or 3
     net = nn.Sequential()
     net:add(nn.Linear(input_size,output_size))
-    for i=1.num_layers do
+    for i=1,num_layers do
         net:add(nn.Linear(output_size,output_size))
     end
+    net:add(nn.Linear(output_size,vector_size))
     net:add(nn.Sigmoid())
+    -- We have the net built up, we need the final cross entropy before we can back prop
+    local margin = self.margin --  set initially 
+    criterion = nn.CosineEmbeddingCriterion(margin)
+    self.criterion = criterion
     self.net = net
+end
+
+function hierarchial_model.forward(input1,y,input2)
+    local network_output = net:forward(input1)
+    local match_output = net:forward(input2)
+    local criterion_input = {}
+    criterion_input[1] = network_output
+    criterion_input[2] = match_output
+    local errval = criterion:forward(criterion_input, y)
+    net = self.net
+    net:zeroGradParameters()
+    local t = criterion:backward(criterion_input, y)
+    net:backward(input1,t[1])
+    net:backward(input2, t[2])
+    net:updateParameters(learningRate)
 end
